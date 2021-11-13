29c6bbf9af75515589c588c81a94a1f14e49f519
Benedikt Neuffer <benedikt.neuffer@partner.kit.edu>
2021-11-13 12:53:49 +0100

    Send probes  more aggressively
    Sending probes every second is to slow. The original revovery tool is probing
    with a frequency of 0.5 seconds. This patch changes probe frequency to 0.1
    seconds.

--- fritzflash.py
+++ fritzflash.py
@@ -138,7 +138,7 @@ def autodiscover_avm_ip():
             i += 1
         except OSError:
             i += 1
-            time.sleep(1)
+            time.sleep(0.1)
         except KeyboardInterrupt:
             print('\rAborting...\n')
             return None
